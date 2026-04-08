1ac34370f7678fdc408697cad40902b4918d41c8
Yutaka Kato <mikan@aomikan.org>
2026-04-08 09:11:16 +0900

    feat: add nikko spots

---
+++ public/routes/spots_utsunomiya.js
@@ -0,0 +1,29 @@
+const utsunomiyaSpots = [
+  {
+    name: "宇都宮宿本陣跡",
+    kana: "うつのみやじゅくほんじんあと",
+    coordinate: [36.561748, 139.87719],
+    icon: "honjin",
+    description:
+      "追分の南側に本陣跡の説明板があります。200坪 (661㎡) 近い大きな建物があったそうですが現存しません。",
+  },
+  {
+    name: "日光街道と奥州街道の追分",
+    kana: "にっこうかいどうとおうしゅうかいどうのおいわけ",
+    coordinate: [36.562016, 139.87708],
+    icon: "monument",
+    description:
+      "日光街道と奥州街道の分岐点で、本陣跡の対面に説明板があります。" +
+      "日光街道はここから北方、奥州街道はここから東方へ道が伸びています。",
+    pictures: [
+      {
+        url: "https://kaidotrail.github.io/img/kagaorange/2024/11-03/PXL_20241103_010720847.webp",
+        comment: "日光街道と奥州街道の追分",
+      },
+      {
+        url: "https://kaidotrail.github.io/img/kagaorange/2024/11-03/PXL_20241103_010726943.webp",
+        comment: "宇陽略記",
+      },
+    ],
+  },
+];
