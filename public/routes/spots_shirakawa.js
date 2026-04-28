b4050696fa05423734f96b62ab871a2558e8fc5f
Yutaka Kato <mikan@aomikan.org>
2026-04-28 22:41:20 +0900

    feat: add oshu spots

--- public/routes/spots_shirakawa.js
+++ public/routes/spots_shirakawa.js
@@ -20,4 +20,181 @@ const shirakawaSpots = [
       },
     ],
   },
+  {
+    name: "奈良屋呉服店",
+    kana: "なやらごふくてん",
+    coordinate: [37.12725, 140.205259],
+    icon: "building",
+    description:
+      "街道筋から門前通りが分岐する角に白壁の美しい呉服店の建物と蔵があります。" +
+      "新町 (しんちょう) と書かれた説明板もあり、宿場の江戸側の入口であり防御の役割を担っていたと解説されています。",
+    pictures: [
+      {
+        url: "https://kaidotrail.github.io/img/kagaorange/2025/01-26/PXL_20250126_052619130.webp",
+        comment: "奈良屋呉服店",
+      },
+      {
+        url: "https://kaidotrail.github.io/img/kagaorange/2025/01-26/PXL_20250126_052632616.MP.webp",
+        comment: "新町 説明板",
+      },
+    ],
+  },
+  {
+    name: "月夜見の庭",
+    kana: "つくよみのにわ",
+    coordinate: [37.128087, 140.205321],
+    icon: "monument",
+    description:
+      "街道筋が直角に折れ曲がる交差点の北東側に白河石が敷き詰められた小さな公園があります。" +
+      "敷地の隅には天神町 (てんじんまち) と書かれた説明板も置かれています。" +
+      "天神神社がその名の由来で、町屋敷の9割以上が商人と職人で構成され城下の商業を担っていたそうです。",
+    pictures: [
+      {
+        url: "https://kaidotrail.github.io/img/kagaorange/2025/01-26/PXL_20250126_052833012.MP.webp",
+        comment: "月夜見の庭",
+      },
+      {
+        url: "https://kaidotrail.github.io/img/kagaorange/2025/01-26/PXL_20250126_052847324.webp",
+        comment: "天神町",
+      },
+    ],
+  },
+  {
+    name: "今井醤油店",
+    kana: "いまいしょうゆてん",
+    coordinate: [37.12801, 140.207392],
+    icon: "building",
+    description:
+      "江戸時代末頃創業の醤油店で、白河市による歴史的風致形成建造物のプレートが設置されています。" +
+      "プレートによると、醤油製造を営む以前は魚屋・乾物屋だったそうです。座敷蔵は明治26年 (1893) に" +
+      "建てられたもので、店舗・主屋は魚屋時代の建造物を増改築して今に至るそうです。",
+    pictures: [
+      {
+        url: "https://kaidotrail.github.io/img/kagaorange/2025/01-26/PXL_20250126_053444785.webp",
+        comment: "今井醤油店",
+      },
+      {
+        url: "https://kaidotrail.github.io/img/kagaorange/2025/01-26/PXL_20250126_053501224.webp",
+        comment: "歴史的風致形成建造物 今井醤油店建造物群",
+      },
+    ],
+  },
+  {
+    name: "鉤形",
+    kana: "かぎがた",
+    coordinate: [37.1282503, 140.2098423],
+    icon: "guide",
+    description:
+      "道筋が二度直角に曲がる鍵形 (桝形) が残っています。" +
+      "北東側の角に案内板が設置されています。",
+    pictures: [
+      {
+        url: "https://kaidotrail.github.io/img/kagaorange/2025/01-26/PXL_20250126_053753439.webp",
+        comment: "鍵形",
+      },
+      {
+        url: "https://kaidotrail.github.io/img/kagaorange/2025/01-26/PXL_20250126_053827666.webp",
+        comment: "奥州街道と白河の城下 説明板",
+      },
+      {
+        url: "https://kaidotrail.github.io/img/kagaorange/2025/01-26/PXL_20250126_053851652.webp",
+        comment: "案内板",
+      },
+    ],
+  },
+  {
+    name: "中町",
+    kana: "なかまち",
+    coordinate: [37.128245, 140.211474],
+    icon: "monument",
+    description:
+      "吉田屋の看板が掛かる建物の横の蔵の前に中町と書かれた説明板があります。" +
+      "かつては城の正面入口にあたる大手 (追手) があったほか、高札場もここ中町にあったそうです。" +
+      "付近には白壁の美しい蔵があります。",
+    pictures: [
+      {
+        url: "https://kaidotrail.github.io/img/kagaorange/2025/01-26/PXL_20250126_054029487.webp",
+        comment: "説明板",
+      },
+      {
+        url: "https://kaidotrail.github.io/img/kagaorange/2025/01-26/PXL_20250126_054125400.webp",
+        comment: "白壁の蔵",
+      },
+    ],
+  },
+  {
+    name: "敷教舎跡",
+    kana: "ふきょうしゃあと",
+    coordinate: [37.127726, 140.214924],
+    icon: "monument",
+    description:
+      "寛政11年 (1799) に白川藩主松平定信が建てた郷学所 (庶民の為の学校) のあった地だそうです。",
+    pictures: [
+      {
+        url: "https://kaidotrail.github.io/img/kagaorange/2025/01-26/PXL_20250126_054500981.webp",
+        comment: "説明板",
+      },
+    ],
+  },
+  {
+    name: "白河宿本陣跡",
+    kana: "しらかわしゅくほんじんあと",
+    coordinate: [37.126843, 140.215492],
+    icon: "honjin",
+    description:
+      "芳賀 (はが) 家が代々務めたという白河宿本陣跡の説明板があります。明治9年 (1876) の明治天皇の巡幸の際には" +
+      "小休所となったそうです。",
+    pictures: [
+      {
+        url: "https://kaidotrail.github.io/img/kagaorange/2025/01-26/PXL_20250126_054708344.webp",
+        comment: "白河宿本陣芳賀家跡",
+      },
+    ],
+  },
+  {
+    name: "白河宿脇本陣柳屋旅館 蔵座敷",
+    kana: "しらかわしゅくわきほんじんやなぎやりょかん くらざしき",
+    coordinate: [37.127187, 140.215707],
+    icon: "building",
+    description:
+      "文久元年 (1804) に建てられた建物が現存しており、公開されています。" +
+      "明治14年 (1881) の明治天皇の巡幸の際には小休所に、還幸の際には行在所になったそうです。" +
+      "建物は蔵造り、内装は屋敷そのもので、随所に凝った造りがみられます。" +
+      "明治天皇も利用したというつるべ井戸も残っています。",
+    url: "https://www.city.shirakawa.fukushima.jp/page/page003774.html",
+    pictures: [
+      {
+        url: "https://kaidotrail.github.io/img/kagaorange/2025/01-26/PXL_20250126_060806971.MP.webp",
+        comment: "蔵座敷",
+      },
+      {
+        url: "https://kaidotrail.github.io/img/kagaorange/2025/01-26/PXL_20250126_054802704.webp",
+        comment: "明治天皇白河行在所跡 説明板",
+      },
+      {
+        url: "https://kaidotrail.github.io/img/kagaorange/2025/01-26/PXL_20250126_060721336.webp",
+        comment: "脇本陣柳屋 説明板",
+      },
+      {
+        url: "https://kaidotrail.github.io/img/kagaorange/2025/01-26/PXL_20250126_054857725.MP.webp",
+        comment: "明治天皇行在所碑",
+      },
+      {
+        url: "https://kaidotrail.github.io/img/kagaorange/2025/01-26/PXL_20250126_055031996.webp",
+        comment: "座敷の様子",
+      },
+      {
+        url: "https://kaidotrail.github.io/img/kagaorange/2025/01-26/PXL_20250126_060305572.webp",
+        comment: "矢羽根天井",
+      },
+      {
+        url: "https://kaidotrail.github.io/img/kagaorange/2025/01-26/PXL_20250126_055150541.webp",
+        comment: "修復工事の概要",
+      },
+      {
+        url: "https://kaidotrail.github.io/img/kagaorange/2025/01-26/PXL_20250126_060428002.webp",
+        comment: "つるべ井戸",
+      },
+    ],
+  },
 ];
