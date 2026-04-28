31e9e818e8af79b7cb1b088e8591d52516ba245a
Yutaka Kato <mikan@aomikan.org>
2026-04-28 21:10:35 +0900

    feat: add oshu spots

---
+++ public/routes/spots_shirakawa.js
@@ -0,0 +1,23 @@
+const shirakawaSpots = [
+  {
+    name: "高湯山道標",
+    kana: "たかゆさんみちしるべ",
+    coordinate: [37.125255, 140.204392],
+    icon: "monument",
+    description:
+      "「左 江戸海道」「高湯山 右 那須原道」と彫られた道標があり、説明板も設置されています。" +
+      "白河市の広報誌によると、高湯山とは那須の茶臼岳から湧き出る温泉をご神体とする霊場で、この道標は" +
+      "高湯山参詣の道案内を兼ねたものと考えられるそうです。",
+    url: "https://www.city.shirakawa.fukushima.jp/data/doc/1734415393_doc_191_0.pdf",
+    pictures: [
+      {
+        url: "https://kaidotrail.github.io/img/kagaorange/2025/01-26/PXL_20250126_052319317.webp",
+        comment: "高湯山道標",
+      },
+      {
+        url: "https://kaidotrail.github.io/img/kagaorange/2025/01-26/PXL_20250126_052309621.webp",
+        comment: "説明板",
+      },
+    ],
+  },
+];
